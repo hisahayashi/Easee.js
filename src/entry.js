16da525009c2037092053cb41389a52aa71f2f0f
Hisayoshi Hayashi <hayashi@hys-inc.jp>
2020-08-13 23:16:01 +0900

    delete global

--- src/entry.js
+++ src/entry.js
@@ -7,11 +7,4 @@
 
 import Easee from './easee.js'
 
-window.Easee = Easee
-let easee = new Easee()
-window.easee = easee
-
 export default Easee
-
-// var p1 = easee.add(1500, 0, 560, 'Linear')
-// console.log(p1)
